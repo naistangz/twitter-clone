471896887fecd07c4304c6d5f6498c2bde8198ce
Anais <a6anaistang@hotmail.co.uk>
2021-04-11 16:59:30 +0100

    conversation views, templates finished

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views
 from django.urls import path
+
+from apps.conversation.views import conversations, conversation
 from apps.core.views import frontpage, signup, login
+
 from apps.feed.views import feed, search
 from apps.feed.api import api_add_oink, api_add_like
 from apps.oinkerprofile.views import oinkerprofile, follow_oinker, unfollow_oinker, followers, follows, edit_profile
@@ -22,6 +25,8 @@ urlpatterns = [
     path('feed/', feed, name='feed'),
     path('search/', search, name='search'),
     path('edit_profile/', edit_profile, name='edit_profile'),
+    path('conversations/', conversations, name='conversations'),
+    path('conversation/<int:user_id>', conversation, name='conversation'),
     path('u/<str:username>/', oinkerprofile, name='oinkerprofile'),
     path('u/<str:username>/follow/', follow_oinker, name='follow_oinker'),
     path('u/<str:username>/followers/', followers, name='followers'),
